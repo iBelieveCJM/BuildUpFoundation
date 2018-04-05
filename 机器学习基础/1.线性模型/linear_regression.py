fdfe5537cd94d6602129689b51083b239cefb1e8
iBelieveCJM <1917066897@qq.com>
2018-04-05 23:19:31 +0800

    linear_regression 添加时间测试装饰器

--- 机器学习基础/1.线性模型/linear_regression.py
+++ 机器学习基础/1.线性模型/linear_regression.py
@@ -6,6 +6,8 @@ Created on Mon Apr  2 18:45:04 2018
 """
 
 import numpy as np
+import time
+from functools import wraps
 
 ####################################################################
 # shuffle data
@@ -29,6 +31,21 @@ def split_data(data, n_train):
     return x_train, y_train, x_test, y_test
 
 ####################################################################
+# decorater to test the execution time
+####################################################################
+
+def time_test(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print('{}ing starting...'.format(func.__name__))
+        start = time.clock()    # use clock() instead of time() in windows
+        result = func(*args, **kwargs)
+        end = time.clock()
+        print('--- {} costs {}'.format(func.__name__, end-start))
+        return result
+    return wrapper
+
+####################################################################
 # my linear regression model
 ####################################################################
 
@@ -38,18 +55,20 @@ class myLinearReg():
         self.w_ = None;
         self.b_ = 0;
         
+    @time_test
     def fit(self, X, y):
         num_x = np.shape(X)[0]
         X_temp = np.concatenate((X, np.ones((num_x, 1))), axis=1)
         self.__closed_form(X_temp, y)
         
+    @time_test
     def predict(self, X):
         return self.w_.dot(X.T) + self.b_
     
     def __closed_form(self, X, y):
-        temp = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
-        self.w_ = temp[:-1]
-        self.b_ = temp[-1]
+        W_temp = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+        self.w_ = W_temp[:-1]
+        self.b_ = W_temp[-1]
 
 ####################################################################
 # main
@@ -60,32 +79,46 @@ from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
 if __name__ == '__main__':
-    ## load data
+    ########## load data ##########
     n_train = 100
+    
+    ## load data
+    start = time.clock()
     iris = load_iris()
+    end = time.clock()
+    print('--- loading data costs {}'.format(end-start))
+    
+    ## processing data
+    start = time.clock()
     shuffle_data(iris)
-    x_train,y_train, x_test,y_test = split_data(iris, n_train)
-    print('loaded data.\nthe number of train data is {}'.format(n_train))
+    x_train, y_train, x_test, y_test = split_data(iris, n_train)
+    end = time.clock()
+    print('--- processing data costs {}'.format(end-start))
+    
+    ## data info
+    print('the number of train data is {}'.format(n_train))
     
     ########## sklearn linear regression ##########
     print('\nsklearn linear regression')
+    reg = linear_model.LinearRegression()
     
     ## training model
-    reg = linear_model.LinearRegression()
-    reg.fit(x_train, y_train)
+    wrap_fit = time_test(reg.fit)
+    wrap_fit(x_train, y_train)
     print('w:',reg.coef_)
     print('b:',reg.intercept_)
     
     ## testing model
-    y_pred = reg.predict(x_test)
+    wrap_predict = time_test(reg.predict)
+    y_pred = wrap_predict(x_test)
     print( 'mean squared error: {}'.format(mean_squared_error(y_test, y_pred)) )
     print( 'Variance score: {}'.format(r2_score(y_test, y_pred)) )
     
     ########## my linear regression ##########
     print('\nmy linear regresssion')
+    myReg = myLinearReg()
     
     ## training model
-    myReg = myLinearReg()
     myReg.fit(x_train, y_train)
     print('w:',myReg.w_)
     print('b:',myReg.b_)
