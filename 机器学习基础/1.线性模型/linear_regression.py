faf77cf21bae01468fe92a2e3c6b7a585269a1af
iBelieveCJM <1917066897@qq.com>
2018-04-22 14:45:10 +0800

    机器学习基础1linear_regression.py 添加 ridge regression

--- 机器学习基础/1.线性模型/linear_regression.py
+++ 机器学习基础/1.线性模型/linear_regression.py
@@ -37,7 +37,7 @@ def split_data(data, n_train):
 def time_test(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        print('{}ing starting...'.format(func.__name__))
+        print('--- {}ing starting...'.format(func.__name__))
         start = time.clock()    # use clock() instead of time() in windows
         result = func(*args, **kwargs)
         end = time.clock()
@@ -54,21 +54,46 @@ class myLinearReg():
     def __init__(self):
         self.w_ = None;
         self.b_ = 0;
+        self.method = {
+                'OLS':lambda X,_: X.T.dot(X),
+                'ridge':lambda X,lambda_: X.T.dot(X) + lambda_*np.eye(np.shape(X)[1])
+                }
         
     @time_test
-    def fit(self, X, y):
+    def fit(self, X, y, method_='OLS', lambda_=0.2):
+        """fit the data
+        method_:
+            'OLS'  : ordinary least squares
+            'ridge': ridge regressioin
+        """
+        # input check
+        if method_ not in self.method:
+            print('error: unknow method')
+            return
+        # data process: w*X+b ==> w_*X_temp, that is w_ = [w,b]
         num_x = np.shape(X)[0]
         X_temp = np.concatenate((X, np.ones((num_x, 1))), axis=1)
-        self.__closed_form(X_temp, y)
+        # XTX checks
+        XTX = self.method[method_](X_temp, lambda_)
+        if np.linalg.det(XTX) == 0:
+            print('error: the X.T.dot(X) is singular')
+            return
+        # if XTX is not singular, it has the closed form solution.
+        print('Call {} regression'.format(method_))
+        self.__closed_form(XTX, X_temp, y)
         
     @time_test
     def predict(self, X):
+        if self.w_ is None:
+            print('error: the model had not been trainedd')
+            return #should raise the expection
         return self.w_.dot(X.T) + self.b_
     
-    def __closed_form(self, X, y):
-        W_temp = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+    def __closed_form(self, XTX, X, y):
+        W_temp = np.linalg.inv(XTX).dot(X.T).dot(y)
         self.w_ = W_temp[:-1]
         self.b_ = W_temp[-1]
+        
 
 ####################################################################
 # main
@@ -80,20 +105,18 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 if __name__ == '__main__':
     ########## load data ##########
-    n_train = 100
+    n_train = 10
     
     ## load data
     start = time.clock()
-    iris = load_iris()
-    end = time.clock()
-    print('--- loading data costs {}'.format(end-start))
+    iris = load_iris()   # the shape of iris.data is (150,4)
+    print('--- loading data costs {}'.format(time.clock()-start))
     
     ## processing data
     start = time.clock()
     shuffle_data(iris)
     x_train, y_train, x_test, y_test = split_data(iris, n_train)
-    end = time.clock()
-    print('--- processing data costs {}'.format(end-start))
+    print('--- processing data costs {}'.format(time.clock()-start))
     
     ## data info
     print('the number of train data is {}'.format(n_train))
