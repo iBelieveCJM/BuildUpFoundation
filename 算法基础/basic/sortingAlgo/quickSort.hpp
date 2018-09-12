88871f3a895ebd6e9d55f99cb9202bea3edcd8c5
iBelieveCJM <1917066897@qq.com>
2018-09-13 00:09:26 +0800

    add swap version

--- 算法基础/basic/sortingAlgo/quickSort.hpp
+++ 算法基础/basic/sortingAlgo/quickSort.hpp
@@ -2,19 +2,20 @@
 #include<vector>
 using std::vector;
 
-int partition(vector<int>& arr, int left, int right){
+int partition0(vector<int>& arr, int left, int right){
     int povit = arr[left];
     std::cout << "pos: " << left << "    povit: " << povit << std::endl;
     while(left < right){   //note. '<' less than, not less and equal(dead loop)
         //note. if no left<right, it will out of range
         //note. there '>=' and next '<', it must have '=' in one condition
         //      or '>' and '<='
+        //note. you must start from right side
         while(arr[right]>=povit && left<right){
             --right;
         }
         arr[left] = arr[right];
         
-        while(arr[left]<povit && left<right){ 
+        while(arr[left]<=povit && left<right){ 
             ++left;
         }
         arr[right] = arr[left];
@@ -23,10 +24,33 @@ int partition(vector<int>& arr, int left, int right){
     return left;          //note. return the position of povit
 }
 
+int partition1(vector<int>& arr, int left, int right){
+    int left_pre = left;   //note. keep the previous left postion
+    int povit = arr[left_pre];
+    while(left < right){   //note. '<' less than, not less and equal(dead loop)
+        while(arr[right]>povit && left<right){  // note. here, the compare operator can be '>' or '>='
+            --right;
+        }
+        while(arr[left]<=povit && left<right){  // note. here, the compare operator must be '<='
+            ++left;
+        }
+        if(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+        }
+    }
+    //note. should put the povit in right place.
+    arr[left_pre] = arr[left];
+    arr[left] = povit;
+    return left;
+}
+
+
 static void quickSort_(vector<int>& arr, int left, int right){
     if(left>=right)
         return;
-    int povit = partition(arr, left, right);
+    int povit = partition0(arr, left, right);
     //std::cout<< "....povit... " << povit << std::endl;
     quickSort_(arr, left, povit-1);  //note. povit-1
     quickSort_(arr, povit+1, right); //note. povit+1
