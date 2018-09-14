d8ae04fcdf449f1affeffdb445c6628f4a501328
iBelieveCJM <1917066897@qq.com>
2018-09-14 12:29:34 +0800

    make algo more general, add a recusive version

--- 算法基础/basic/Backtracking/eightQueen.cpp
+++ 算法基础/basic/Backtracking/eightQueen.cpp
@@ -9,22 +9,25 @@ void print(int x){
     std::cout<< x << "  ";
 }
 void printVec(vector<int>& arr){
-    for_each(arr.begin(), arr.end(), print); std::cout<<std::endl;
-}
-bool is_not_suit(vector<int>& pos, int step){
-    bool ret = false;
-    for(int i=0; i<step; ++i){
-        ret = (ret || (pos[i]==pos[step]) || (abs(pos[i]-pos[step])==abs(i-step)) );
-    }
-    return ret;
+    for_each(arr.begin(), arr.end(), print);
+    std::cout<<std::endl;
 }
 
-void eightQueen(int num){
+bool is_ok(vector<int>& pos, int step){
+    for(int i=0; i<step; ++i){
+        if( (pos[i]==pos[step]) || (abs(pos[i]-pos[step])==abs(i-step)) ){
+            return false;
+        }
+    }
+    return true;
+}
+
+void Queen1(int num){
     vector<int> pos(num, 0);
     int step = 0;
     int count = 0;
     while(pos[0]!=num){
-        if(step>0 && is_not_suit(pos, step)){
+        if(step>0 && (!is_ok(pos, step)) ){
             ++pos[step];
         }
         else{
@@ -46,7 +49,29 @@ void eightQueen(int num){
     std::cout<< "total solutions: " << count << std::endl;
 }
 
+
+void queen_(vector<int>& pos, int col, int num, int& count){
+    if(col == num){
+        count++;
+        printVec(pos);
+    }
+    else{
+        for(int row=0; row!=num; ++row){
+            pos[col] = row;
+            if( is_ok(pos, col) ){
+                queen_(pos, col+1, num, count);
+            }
+        }
+    }
+}
+void Queen2(int num){
+    int count = 0;
+    vector<int> pos(num, 0);
+    queen_(pos, 0, num, count);
+    std::cout<< "total solutions: " << count << std::endl;
+}
+
 int main(){
-    eightQueen(8);
+    Queen2(8);
     return 0;
 }
