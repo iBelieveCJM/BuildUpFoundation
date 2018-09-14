d59f0618d22b1f28b742b248b16b93285ccbd0fd
iBelieveCJM <1917066897@qq.com>
2018-09-14 21:41:46 +0800

    make partial permutation

--- 算法基础/basic/Backtracking/permutation.cpp
+++ 算法基础/basic/Backtracking/permutation.cpp
@@ -6,46 +6,52 @@
 #include<stdlib.h>
 using std::vector;
 
-template<typename T>
-void printVec(vector<T>& arr){
-    for(T e: arr) std::cout<< e << "  ";
-    std::cout<<std::endl;
-}
-
-template<typename T>
-void swap(T& a, T& b){
-    if(&a == &b){
-        return;
-    }
-    T temp = a;
-    a = b;
-    b = temp;
-}
-
 #ifdef DEBUG
 int count = 0;
 #endif
 
 template<typename T>
-void fullPermutation(vector<T>& arr, int pos){
-    if(pos == (arr.size()-1) ){
-        printVec(arr);
+void printPartialVec(vector<T>& arr, vector<int>& pos){
+    for(int i=0; i<pos.size(); ++i){
+        std::cout<< arr[pos[i]] << "  ";
+    }        
+    std::cout<<std::endl;
+}
+bool is_ok(vector<int>& pos, int step){
+    for(int i=0; i<step; ++i){
+        if(pos[i] == pos[step]){
+            return false;
+        }
+    }
+    return true;
+}
+template<typename T>
+void permutation_(vector<T>& arr, vector<int>& pos, int step){
+    if(step == pos.size() ){
+        printPartialVec(arr, pos);
 #ifdef DEBUG
         ++count;
 #endif
-        return;
     }
-    for(int i=pos; i<arr.size(); ++i){
-        swap(arr[pos], arr[i]);
-        fullPermutation(arr, pos+1);
-        swap(arr[pos], arr[i]);
+    else{
+        for(int i=0; i!=arr.size(); ++i){
+            pos[step] = i;
+            if( is_ok(pos, step) ){
+                permutation_(arr, pos, step+1);
+            }
+        }
     }
+}
+template<typename T>
+void permutation(vector<T>& arr, int n){
+    vector<int> pos(n, 0);
+    permutation_(arr, pos, 0);
 }
 
 int main(){
     //vector<int> arr = {2,3,4};
     vector<char> arr = {'a', 'b', 'c', 'd'};
-    fullPermutation(arr, 0);
+    permutation(arr, 1);
 #ifdef DEBUG
     std::cout << "total solution: " << count << std::endl;
 #endif
