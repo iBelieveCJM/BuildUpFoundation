1fc6a901d882e7d0ee873747079d85e24aded821
iBelieveCJM <1917066897@qq.com>
2018-09-16 11:22:05 +0800

    added version for swaping node

--- 算法基础/basic/sortingAlgo/quickSort_link.hpp
+++ 算法基础/basic/sortingAlgo/quickSort_link.hpp
@@ -11,6 +11,23 @@ void swap_val(linkNode* a, linkNode* b){
     a->val = b->val;
     b->val = temp;
 }
+void swap_node(linkNode* pre_a, linkNode* a, linkNode* pre_b, linkNode* b){
+    if(a == b){
+        return;
+    }
+    if(pre_b == a){
+        linkNode* after_b = b->next;
+        b->next = a;
+        a->next = after_b;
+        pre_a->next = b;
+        return;
+    }
+    linkNode* after_b = b->next;
+    b->next = a->next;
+    a->next = after_b;
+    pre_a->next = b;
+    pre_b->next = a;
+}
 ///////////////////
 // the idea seems like remove the repeated elements in a ordered array
 ///////////////////
@@ -29,14 +46,70 @@ linkNode* partition_val(linkNode* left, linkNode* right){
     return povit;
 }
 
-static void quickSort_link_(linkNode* left, linkNode* right){
+///////////////////
+// the idea is to create two list: 
+//    0.list for element which less than povit
+//    1.list for element which greater than povit
+// then link two list and other part of the whole list.
+///////////////////
+void insertList(linkNode** list, linkNode** point){
+    (*point)->next = (*list)->next;
+    (*list)->next = *point;
+    *list = *point;
+}
+linkNode* partition_node(linkNode* pre_left, linkNode* left, linkNode* right){
+    linkNode* point = left;
+    linkNode* povit = left;
+    linkNode povitHead = {0, nullptr};
+    linkNode* povitList = &povitHead;
+    linkNode lessHead = {0, nullptr};
+    linkNode* lessList = &lessHead;
+    linkNode greaterHead = {0, nullptr};
+    linkNode* greaterList = &greaterHead;
+    while(point != right){
+        linkNode* aft = point->next;
+        
+        if( point->val < povit->val ){
+            insertList(&lessList, &point);   // if you want to change the pointer, you should use pointer to pointer
+        }
+        if( point->val > povit->val ){
+            insertList(&greaterList, &point);
+        }
+        if( point->val == povit->val){
+            insertList(&povitList, &point);
+        }
+        
+        point = aft;
+    }
+    greaterList->next = right;
+    povitList->next = greaterHead.next;
+    lessList->next = povitHead.next;
+    pre_left->next = lessHead.next;
+    return povit;
+}
+
+static void quickSort_link_val_(linkNode* left, linkNode* right){
     if(left == right)
         return;
     linkNode* povit = partition_val(left, right);
-    quickSort_link_(left, povit);
-    quickSort_link_(povit->next, right);
+    std::cout<< "povit: " << povit->val << std::endl;
+    quickSort_link_val_(left, povit);
+    quickSort_link_val_(povit->next, right);
+}
+
+static void quickSort_link_node_(linkNode* pre_left, linkNode* left, linkNode* right){
+    if(left == right)
+        return;
+    linkNode* povit = partition_node(pre_left, left, right);
+    std::cout<< "left: " << pre_left->next->val << " povit: " << povit->val << std::endl;
+    quickSort_link_node_(pre_left, pre_left->next, povit);
+    quickSort_link_node_(povit, povit->next, right);
 }
 
 void quickSort_link(linkNode* head){
-    quickSort_link_(head->next, nullptr);
+    if(head==nullptr || head->next==nullptr){
+        return;
+    }
+    //quickSort_link_val_(head->next, nullptr);
+    quickSort_link_node_(head, head->next, nullptr);
 }
