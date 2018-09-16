088afb6a7bc54f1a13ba73d3d659681f560a984f
iBelieveCJM <1917066897@qq.com>
2018-09-16 11:22:43 +0800

    add test case for repetition

--- 算法基础/basic/sortingAlgo/testCaseForLink.cpp
+++ 算法基础/basic/sortingAlgo/testCaseForLink.cpp
@@ -13,17 +13,71 @@ void testUnorderedOdd();
 void testUnorderedEven();
 void testInvOdd();
 void testInvEven();
+void testRepetitionOdd();
+void testRepetitionEven();
+
+void testSwapNode1(){
+    dynamicLink testlink = {9};
+    linkNode* head = testlink.getHead();
+    swap_node(head, head->next, head, head->next);
+    testlink.printLink();
+}
+
+void testSwapNode2(){
+    dynamicLink testlink = {9, 8};
+    linkNode* head = testlink.getHead();
+    swap_node(head, head->next, head->next, head->next->next);
+    testlink.printLink();
+}
+
+void testSwapNode3(){
+    dynamicLink testlink = {9, 8, 5};
+    linkNode* head = testlink.getHead();
+    swap_node(head, head->next, head->next->next, head->next->next->next);
+    testlink.printLink();
+}
+
+void testSwapNode4(){
+    dynamicLink testlink = {9, 8, 5, 4};
+    linkNode* head = testlink.getHead();
+    swap_node(head, head->next, head->next->next->next, head->next->next->next->next);
+    testlink.printLink();
+}
+
+void testSwapNode4_2(){
+    dynamicLink testlink = {9, 8, 5, 4};
+    linkNode* head = testlink.getHead();
+    swap_node(head, head->next, head->next->next, head->next->next->next);
+    testlink.printLink();
+}
+
+void testSwapNode4_3(){
+    dynamicLink testlink = {9, 8, 5, 4};
+    linkNode* head = testlink.getHead();
+    swap_node(head->next, head->next->next, head->next->next->next, head->next->next->next->next);
+    testlink.printLink();
+}
 
 int main(){
+    //testSwapNode1();
+    //testSwapNode2();
+    //testSwapNode3();
+    //testSwapNode4();
+    //testSwapNode4_2();
+    //testSwapNode4_3();
+    
     testUnorderedOdd();
     testUnorderedEven();
     testInvOdd();
     testInvEven();
+    testRepetitionOdd();
+    testRepetitionEven();
     return 0;
 }
 
 void testUnorderedOdd(){
     dynamicLink testlink = {9, 4, 111, 0, -4, 12, -8};
+    testlink.printLink();
     dynamicLink verlink = {-8, -4, 0, 4, 9, 12, 111};
     sort(testlink.getHead());
     if(verlink.equals(testlink)){
@@ -60,6 +114,26 @@ void testInvEven(){
     sort(testlink.getHead());
     if(verlink.equals(testlink)){
         std::cout<< "sorted the inv and even link successfully" << std::endl;
+    }
+    testlink.printLink();
+}
+
+void testRepetitionOdd(){
+    dynamicLink testlink = {9, 4, 111, 0, 0, -4, 12};
+    dynamicLink verlink = {-4, 0, 0, 4, 9, 12, 111};
+    sort(testlink.getHead());
+    if(verlink.equals(testlink)){
+        std::cout<< "sorted the odd link which has repeated elements successfully" << std::endl;
+    }
+    testlink.printLink();
+}
+
+void testRepetitionEven(){
+    dynamicLink testlink = {9, 4, 111, 0, 0, -4, 12, 0};
+    dynamicLink verlink = {-4, 0, 0, 0, 4, 9, 12, 111};
+    sort(testlink.getHead());
+    if(verlink.equals(testlink)){
+        std::cout<< "sorted the even link which has repeated elements successfully" << std::endl;
     }
     testlink.printLink();
 }
