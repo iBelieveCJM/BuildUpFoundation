f1d7fdf205f95a4173b50d060462d5aa9089ea69
iBelieveCJM <1917066897@qq.com>
2018-12-31 09:37:34 +0800

    fix a bug: l2-norm not work in bias term

--- 机器学习基础/1.线性模型/linear_regression_l2.py
+++ 机器学习基础/1.线性模型/linear_regression_l2.py
@@ -14,20 +14,22 @@ class LinearRegL2():
         
     def fit(self, X, y):
         # data process
-        #   w*X+b ==> w_*X_temp, that is w_ = [w,b]
+        #   w*X+b ==> w_*X_temp, that is w_=[b,w], X_temp=[1,X]
         num_x = np.shape(X)[0]
-        X_temp = np.concatenate((X, np.ones((num_x, 1))), axis=1)
+        X_temp = np.concatenate((np.ones((num_x, 1)), X), axis=1)
         
         # singular checks
-        XTX = X_temp.T.dot(X_temp) + self.lambda_*np.eye(np.shape(X_temp)[1])
+        identity = np.eye(np.shape(X_temp)[1])
+        identity[0,0] = 0
+        XTX = X_temp.T.dot(X_temp) + self.lambda_*identity
         if np.linalg.det(XTX) == 0:
             print('error: the X.T.dot(X) is singular')
             return #should raise the expection
             
         # if XTX is not singular, it has the closed form solution.
         W_temp = np.linalg.inv(XTX).dot(X_temp.T).dot(y)
-        self.coef_ = W_temp[:-1]
-        self.intercept_ = W_temp[-1]
+        self.coef_ = W_temp[1:]
+        self.intercept_ = W_temp[0]
         
     def predict(self, X):
         if self.coef_ is None:
