f14cdf6d8d7c42e2ff5d145a1db6ef2d55b7de34
iBelieveCJM <1917066897@qq.com>
2019-01-09 00:16:04 +0800

    fix the bug_v0 of C4_5: continue values

--- 机器学习基础/2.决策树/C4_5.py
+++ 机器学习基础/2.决策树/C4_5.py
@@ -4,30 +4,58 @@
 """
 
 import numpy as np
+import pandas as pd
 from collections import Counter
+from pandas.api.types import is_integer, is_categorical_dtype, is_object_dtype, is_string_dtype
+
+DEBUG = True
+eps = 1e-8
 
 class C45Tree():
     
     def __init__(self):
         self.tree = None
+        self.continueFeatVals = None
+        self.labelDtype = None
     
     def fit(self, data, labels):
-        self.tree = self.createTree(data, labels, [])
+        self.labelDtype = labels.dtype
+        featName = self.makeFeatName(data)
+        self.continueFeatVals = {feat:0 for feat, dtype in featName.items()
+                                  if dtype=='continue'}
+        print(featName)
+        print(self.continueFeatVals)
+        self.tree = self.createTree(data, labels, featName)
         
     def predict(self, data):
-        ret = np.zeros(data.shape[0])
-        for idx, sample in enumerate(data):
-            ret[idx] = self.classify(self.tree, sample)
+        ret = pd.Series(-1, range(len(data)), dtype=self.labelDtype)
+        for idx in range(len(data)):
+            ret[idx] = self.classify(self.tree, data.iloc[idx])
         return ret
         
     def classify(self, tree, sample):
-        if isinstance(tree, np.int32):
+        if is_integer(tree):
             return tree
         else:
-            axis = list(tree.keys())[0]
-            return self.classify(tree[axis][sample[axis]], sample)
+            feat = list(tree.keys())[0]
+            if feat in self.continueFeatVals:
+                if sample[feat] > self.continueFeatVals[feat]:
+                    ret = None
+                    try:
+                        ret = self.classify(tree[feat]['>'], sample)
+                    except KeyError:
+                        print('---- keyError ----' )
+                        print('featName: ', feat)
+                        print(f'sample[feat]: {sample[feat]}, '\
+                              f'self.continueFeatVals[feat]:  {self.continueFeatVals[feat]}')
+                        print(tree[feat])
+                    return ret
+                else:
+                    return self.classify(tree[feat]['<='], sample)
+            else:
+                return self.classify(tree[feat][sample[feat]], sample)
         
-    def calcEnt(self, data, labels):
+    def calcEnt(self, labels):
         unique_labels = Counter(labels)
         datalen = labels.shape[0]
         ent = 0.0
@@ -36,39 +64,104 @@ class C45Tree():
             ent -= prob*np.log(prob)
         return ent
     
-    def bestFeat(self, data, labels, order):
-        bestFeatAxis, bestEnt = -1, 0.0
-        origin_ent = self.calcEnt(data, labels)
-        num, axises = data.shape
-        for axis in range(axises):
-            if axis not in order:
-                values = set(data[:,axis])
-                ent_for_partition, ent_split = 0.0, 0.0
-                for val in values:
-                    ent_for_val = self.calcEnt(data[data[:,axis]==val],
-                                               labels[data[:,axis]==val])
-                    prob_for_val = np.sum(data[:,axis]==val)/num
-                    ent_for_partition += (prob_for_val * ent_for_val)
-                    ent_split -= prob_for_val*np.log(prob_for_val)
-                ent_inc_rate = (origin_ent - ent_for_partition) / ent_split
-                if ent_inc_rate > bestEnt:
-                    bestEnt = ent_inc_rate
-                    bestFeatAxis = axis
-        return bestFeatAxis
+    def calcDiscreteEntIncRate(self, featData, labels, origin_ent):
+        values = set(featData)
+        ent_for_partition, ent_split = 0.0, 0.0
+        for val in values:
+            ent_for_val = self.calcEnt(labels[featData==val])
+            prob_for_val = np.sum(featData==val)/featData.shape[0]
+            ent_for_partition += (prob_for_val * ent_for_val)
+            ent_split -= prob_for_val*np.log(prob_for_val)
+        ent_inc_rate = (origin_ent - ent_for_partition) / (ent_split+eps)
+        return ent_inc_rate, None
     
-    def createTree(self, data, labels, order):
+    def calcContinueEntIncRate(self, featData, labels, origin_ent):
+        values = featData.unique()
+        if values.shape[0] == 1:
+            return 0.0, values[0]
+        midVals = (values[:-1]+values[1:])/2.0
+        bestEntIncRate, bestVal = 0.0, midVals[0]
+        for val in midVals:
+            ent_for_partition, ent_split = 0.0, 0.0
+            # greater than
+            ent_greater = self.calcEnt(labels[featData>val])
+            prob_greater = np.sum(featData>val)/featData.shape[0]
+            ent_for_partition += prob_greater*ent_greater
+            ent_split -= prob_greater*np.log(prob_greater+eps)
+            # less and equal than
+            ent_lessEqual = self.calcEnt(labels[featData<=val])
+            prob_lessEqual = np.sum(featData<=val)/featData.shape[0]
+            ent_for_partition += prob_lessEqual*ent_lessEqual
+            ent_split -=  prob_lessEqual*np.log(ent_lessEqual+eps)
+            # entropy increase rate
+            ent_inc_rate = (origin_ent - ent_for_partition) / (ent_split+eps)
+            if ent_inc_rate > bestEntIncRate:
+                bestEntIncRate = ent_inc_rate
+                bestVal = val
+        return bestEntIncRate, bestVal
+    
+    def bestFeat(self, data, labels, featName):
+        if DEBUG: print('-'*30, '\nall name: ', featName)
+#        if len(featName)==1:
+#            return featName[0], 0
+        bestFeatVal, bestEntIncRate = -1, -0.1
+        origin_ent = self.calcEnt(labels)
+        bestFeatName = None
+        for col, dtype in featName.items():
+            if dtype == 'discrete':
+                ent_inc_rate, val = self.calcDiscreteEntIncRate(data[col], labels, origin_ent)
+                if DEBUG: print('discrete feat: ', col, ' rate: ', ent_inc_rate)
+            else:                
+                ent_inc_rate, val = self.calcContinueEntIncRate(data[col], labels, origin_ent)
+                if DEBUG: print('continue feat: ', col, ' rate: ', ent_inc_rate, ' val: ', val)
+            if ent_inc_rate > bestEntIncRate:
+                bestEntIncRate = ent_inc_rate
+                bestFeatName, bestFeatVal = col, val
+        return bestFeatName, bestFeatVal
+    
+    def createTree(self, data, labels, featName):
         #End condition 1: only one class left.
         if len(set(labels))==1:
-            return labels[0]
-        bestFeature = self.bestFeat(data, labels, order)
-        order.append(bestFeature)
-        values = set(data[:,bestFeature])
-        #End condition 2: only one feature left.
-        if len(values)==1:
-            return Counter(data[:,bestFeature]).most_common(1)[1] #majority voting
-        tree = {bestFeature:{}}
-        for val in values:
-            tree[bestFeature][val] = self.createTree(data[data[:,bestFeature]==val],
-                                                     labels[data[:,bestFeature]==val],
-                                                     [x for x in order])
+            return labels.iloc[0]
+        #End condition 2: not feature left ot split.
+        if len(featName)==0:
+            return Counter(labels).most_common(1)[0][0] #majority voting
+        #Recursive create tree
+        bestFeatName, bestFeatVal = self.bestFeat(data, labels, featName)
+        if bestFeatName in self.continueFeatVals: 
+            self.continueFeatVals[bestFeatName]=bestFeatVal
+        if DEBUG: print('best feature: ', bestFeatName, ' val: ', bestFeatVal)
+        del featName[bestFeatName]
+#        #End condition 3: only one feature value left.
+#        if len(values)==1:
+#            return Counter(labels).most_common(1)[0][0]
+        tree = {bestFeatName:{}}
+        if bestFeatVal is None:
+            values = set(data[bestFeatName])
+            if DEBUG: print('best feature valuse: ', values)
+            for val in values:
+                tree[bestFeatName][val] = self.createTree(data[data[bestFeatName]==val],
+                                                          labels[data[bestFeatName]==val],
+                                                          featName.copy())
+        else:
+            tree[bestFeatName]['<='] = self.createTree(data[data[bestFeatName]<=bestFeatVal],
+                                                       labels[data[bestFeatName]<=bestFeatVal],
+                                                       featName.copy())
+            if not data[bestFeatName].unique().shape[0]==1:
+                tree[bestFeatName]['>'] = self.createTree(data[data[bestFeatName]>bestFeatVal],
+                                                          labels[data[bestFeatName]>bestFeatVal],
+                                                          featName.copy())
+            else:
+                tree[bestFeatName]['>'] = Counter(labels).most_common(1)[0][0] #majority voting
         return tree
+    
+    def makeFeatName(self, data):
+        featName = {}
+        for col in data:
+            if is_categorical_dtype(data[col]) or\
+               is_object_dtype(data[col]) or\
+               is_string_dtype(data[col]):
+                featName[col] = 'discrete'
+            else:
+                featName[col] = 'continue'
+        return featName
--- 机器学习基础/2.决策树/testCase.py
+++ 机器学习基础/2.决策树/testCase.py
@@ -6,10 +6,10 @@
 import numpy as np
 import pandas as pd
 from id3 import ID3Tree
-#from C4_5 import C45Tree
+from C4_5 import C45Tree
 
-def testID31():
-    print('-'*30, '\ntestID31\n', '-'*30)
+def test1(t):
+    print('-'*30, '\ntest1\n', '-'*30)
     data = np.array(
             [[1,1,1],
              [1,1,1],
@@ -17,23 +17,21 @@ def testID31():
              [2,1,2],
              [2,1,2]])
     data = pd.DataFrame(data, columns=list('abc'))
-    data.apply(lambda x: x.astype('category'))
+    data = data.apply(lambda x: x.astype('category'))
     #for col in list('abc'):
     #    data[col] = data[col].astype('category')
     print(data)
     labels = pd.Series(np.array([1,1,2,0,0]), name='labels')
     print(labels)
     
-    id3Tree = ID3Tree()
-    #id3Tree = C45Tree()
-    id3Tree.fit(data, labels)
-    print(id3Tree.tree)
-    pred = id3Tree.predict(data)
+    t.fit(data, labels)
+    print(t.tree)
+    pred = t.predict(data)
     print(pred)
     print('Acc.: ', np.sum(pred==labels)/labels.shape[0])
     
-def testID32():
-    print('-'*30, '\ntestID32\n', '-'*30)
+def test2(t):
+    print('-'*30, '\ntest2\n', '-'*30)
     data = np.array(
             [[1,1],
              [1,1],
@@ -41,34 +39,28 @@ def testID32():
              [2,1],
              [2,1]])
     data = pd.DataFrame(data, columns=list('ab'))
+    data = data.apply(lambda x: x.astype('category'))
     print(data)
     labels = pd.Series(np.array([1,1,2,0,0]), name='labels')
     print(labels)
     
-    id3Tree = ID3Tree()
-    #id3Tree = C45Tree()
-    id3Tree.fit(data, labels)
-    print(id3Tree.tree)
-    pred = id3Tree.predict(data)
+    t.fit(data, labels)
+    print(t.tree)
+    pred = t.predict(data)
     print('Acc.: ', np.sum(pred==labels)/labels.shape[0])
                 
-def testTitanic():
-    print('-'*30, '\ntestTitanic\n', '-'*30)
+def testTitanicDiscrete(t):
+    print('-'*30, '\ntestTitanicDiscrete\n', '-'*30)
     trd = pd.read_csv('Titanic_dataset/train.csv')
     # drop useless and continue features
     for i in ["PassengerId", "Name", "Ticket", "Cabin", "Age", "SibSp", "Parch", "Fare"]:
         trd.pop(i)
-    # drop nan values
-    trd = trd.dropna()
-    # using "Survived" as labels
-    trl = trd.pop("Survived")
-    id3Tree = ID3Tree()
-    #id3Tree = C45Tree()
-    id3Tree.fit(trd, trl)
-    print(id3Tree.tree)
-    # test train data
-    pred = id3Tree.predict(trd)
-    #print(pred)
+    trd = trd.dropna()        # drop nan values
+    trl = trd.pop("Survived") # using "Survived" as labels
+    
+    t.fit(trd, trl)
+    print(t.tree)
+    pred = t.predict(trd)
     print('Acc.: ', np.sum(pred==trl.reset_index(drop=True))/trl.shape[0])
     
 #    # test test data
@@ -76,9 +68,35 @@ def testTitanic():
 #    for i in ["PassengerId", "Name", "Ticket", "Cabin", "Age", "SibSp", "Parch", "Fare"]:
 #        ttd.pop(i)
 #    ttd = ttd.dropna()
-#    pred = id3Tree.predict(ttd)
+#    pred = t.predict(ttd)
+    
+def testTitanicC4_5(t):
+    print('-'*30, '\ntestTitanicC4_5\n', '-'*30)
+    trd = pd.read_csv('Titanic_dataset/train.csv')
+    # drop useless features
+    for i in ["PassengerId", "Name", "Ticket", "Cabin"]:
+        trd.pop(i)
+    trd = trd.dropna()        # drop nan values
+    trd['Pclass'] = trd['Pclass'].astype('category')
+    #trd = trd[:100]
+    print(trd.shape)
+    trl = trd.pop("Survived") # using "Survived" as labels
+    t.fit(trd, trl)
+    print(t.tree)
+    pred = t.predict(trd)
+    print('Acc.: ', np.sum(pred==trl.reset_index(drop=True))/trl.shape[0])
+    
+#    # test test data
+#    ttd = pd.read_csv('Titanic_dataset/test.csv')
+#    for i in ["PassengerId", "Name", "Ticket", "Cabin", "Age", "SibSp", "Parch", "Fare"]:
+#        ttd.pop(i)
+#    ttd = ttd.dropna()
+#    pred = t.predict(ttd)
     
 if __name__ == '__main__':
-    testID31()
-    testID32()
-    testTitanic()
+    #t = ID3Tree()
+    t = C45Tree()
+    #test1(t)
+    #test2(t)
+    #testTitanicDiscrete(t)
+    testTitanicC4_5(t)
